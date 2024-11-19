67c5cd0feab5b98d260d3ebf76e112886de39325
hunn99 <dennymalikibrahim@gmail.com>
2024-11-19 10:06:16 +0700

    [feat] Add button widget

---
+++ lib/presentation/widgets/buttons/button.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:looksy_app/presentation/utils/text.dart';
+import 'package:looksy_app/presentation/utils/theme.dart';
+
+class LargeFillButton extends StatelessWidget {
+  final String label;
+  final double? width;
+  final VoidCallback onPressed;
+  final bool? isDisabled;
+
+  const LargeFillButton(
+      {super.key,
+      required this.label,
+      required this.onPressed,
+      this.width,
+      this.isDisabled});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled != true ? onPressed : null,
+      child: Container(
+        height: 48,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: width ?? double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: isDisabled != true ? neutralTheme: neutralTheme[300],
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style: bodyBlack1.copyWith(color: neutralTheme),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SmallFillButton extends StatelessWidget {
+  final String label;
+  final double? width;
+  final VoidCallback onPressed;
+  final bool? isDisabled;
+
+  const SmallFillButton(
+      {super.key,
+      required this.label,
+      required this.onPressed,
+      this.width,
+      this.isDisabled});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled != true ? onPressed : null,
+      child: Container(
+        height: 36,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: width ?? double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: isDisabled != true ? neutralTheme : neutralTheme[300],
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style: bodyBlack1.copyWith(color: neutralTheme),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CancelButton extends StatelessWidget {
+  final String label;
+  final double? width;
+  final VoidCallback onPressed;
+  final bool? isDisabled;
+
+  const CancelButton(
+      {super.key,
+      required this.label,
+      required this.onPressed,
+      this.width,
+      this.isDisabled});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled != true ? onPressed : null,
+      child: Container(
+        // height: 36,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: width ?? double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(color: isDisabled != true ? redTheme[600]! : neutralTheme[300]!),
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style: bodyBlack1.copyWith(
+                color: isDisabled != true ? redTheme[600] : neutralTheme[300]),
+          ),
+        ),
+      ),
+    );
+  }
+}
