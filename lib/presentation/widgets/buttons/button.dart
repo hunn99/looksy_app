f10f11513fc87e6d0f5e2e836a3a140edabebb72
hunn99 <dennymalikibrahim@gmail.com>
2024-11-20 16:50:30 +0700

    [feat] Widget button

--- lib/presentation/widgets/buttons/button.dart
+++ lib/presentation/widgets/buttons/button.dart
@@ -5,10 +5,54 @@ import 'package:looksy_app/presentation/utils/theme.dart';
 class LargeFillButton extends StatelessWidget {
   final String label;
   final double? width;
+  final VoidCallback? onPressed;
+  final bool isDisabled;
+  final bool isLoading;
+
+  const LargeFillButton({
+    super.key,
+    required this.label,
+    this.width,
+    this.onPressed,
+    this.isDisabled = false,
+    this.isLoading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled || isLoading ? null : onPressed,
+      child: Container(
+        height: 48,
+        width: width ?? double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: (isDisabled || isLoading) ? neutralTheme[300] : neutralTheme,
+        ),
+        child: Center(
+          child: isLoading
+              ? const CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeWidth: 2,
+                )
+              : Text(
+                  label,
+                  style: bodyBlack1.copyWith(color: Colors.white),
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class LargeFillButtonWhite extends StatelessWidget {
+  final String label;
+  final double? width;
   final VoidCallback onPressed;
   final bool? isDisabled;
 
-  const LargeFillButton(
+  const LargeFillButtonWhite(
       {super.key,
       required this.label,
       required this.onPressed,
@@ -25,7 +69,7 @@ class LargeFillButton extends StatelessWidget {
         width: width ?? double.infinity,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(100),
-          color: isDisabled != true ? neutralTheme: neutralTheme[300],
+          color: isDisabled != true ? Colors.white : neutralTheme[300],
         ),
         child: Center(
           child: Text(
@@ -97,7 +141,8 @@ class CancelButton extends StatelessWidget {
         width: width ?? double.infinity,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(100),
-          border: Border.all(color: isDisabled != true ? redTheme[600]! : neutralTheme[300]!),
+          border: Border.all(
+              color: isDisabled != true ? redTheme[600]! : neutralTheme[300]!),
         ),
         child: Center(
           child: Text(
