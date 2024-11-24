4cce03358bf7b74f5ee205754ba2566f7ea6c832
hunn99 <dennymalikibrahim@gmail.com>
2024-11-24 20:13:05 +0700

    [fix] Navigation page

--- lib/presentation/pages/navigation/navigation.dart
+++ lib/presentation/pages/navigation/navigation.dart
@@ -9,28 +9,19 @@ import 'package:looksy_app/presentation/pages/tips/tips.dart';
 import 'package:looksy_app/presentation/utils/theme.dart';
 import 'package:looksy_app/presentation/widgets/navigations/menu_items.dart';
 
-// ignore: must_be_immutable
 class NavigationPage extends StatefulWidget {
-  int? selectedIndex;
-  NavigationPage({super.key, this.selectedIndex = 0});
+  const NavigationPage({super.key});
 
   @override
   State<NavigationPage> createState() => _NavigationPageState();
 }
 
 class _NavigationPageState extends State<NavigationPage> {
-  final List<String> _routesName = [
-    '/home',
-    '/home/tips',
-    '/home/scan',
-    '/home/history',
-    '/home/profile'
-  ];
+  int? selectedIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
-      widget.selectedIndex = index;
-      context.go(_routesName[index]);
+      selectedIndex = index;
     });
   }
 
@@ -39,7 +30,6 @@ class _NavigationPageState extends State<NavigationPage> {
     final List<Widget> pages = <Widget>[
       const HomePage(),
       const TipsPage(),
-      const ScanPage(),
       const HistoryPage(),
       const ProfilePage(),
     ];
@@ -47,61 +37,83 @@ class _NavigationPageState extends State<NavigationPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: pages[widget.selectedIndex!],
+        body: pages[selectedIndex!],
         bottomNavigationBar: Container(
-            height: 72,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border(
-                top: BorderSide(color: neutralTheme[50]!, width: 1),
-              ),
+          height: 72,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(color: neutralTheme[50]!, width: 1),
             ),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Row(
-              children: [
-                MenuItem(
-                  label: 'Home',
-                  selectedIcon: IconsaxBold.home_1,
-                  unselectedIcon: IconsaxOutline.home,
-                  index: 0,
-                  selectedPageIndex: widget.selectedIndex ?? 0,
-                  onTap: _onItemTapped,
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MenuItem(
+                label: 'Home',
+                selectedIcon: IconsaxBold.home_1,
+                unselectedIcon: IconsaxOutline.home,
+                index: 0,
+                selectedPageIndex: selectedIndex!,
+                onTap: _onItemTapped,
+              ),
+              MenuItem(
+                label: 'Tips',
+                selectedIcon: IconsaxBold.magicpen,
+                unselectedIcon: IconsaxOutline.magicpen,
+                index: 1,
+                selectedPageIndex: selectedIndex!,
+                onTap: _onItemTapped,
+              ),
+              const SizedBox(width: 80),
+              MenuItem(
+                label: 'History',
+                selectedIcon: IconsaxBold.document_text,
+                unselectedIcon: IconsaxOutline.document_text,
+                index: 2,
+                selectedPageIndex: selectedIndex!,
+                onTap: _onItemTapped,
+              ),
+              MenuItem(
+                label: 'Profile',
+                selectedIcon: IconsaxBold.user,
+                unselectedIcon: IconsaxOutline.user,
+                index: 3,
+                selectedPageIndex: selectedIndex!,
+                onTap: _onItemTapped,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          padding: const EdgeInsets.only(top: 20),
+          child: FloatingActionButton(
+            onPressed: () {
+              // Arahkan ke halaman scan
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ScanPage(),
                 ),
-                MenuItem(
-                  label: 'Tips',
-                  selectedIcon: IconsaxBold.magicpen,
-                  unselectedIcon: IconsaxOutline.magicpen,
-                  index: 1,
-                  selectedPageIndex: widget.selectedIndex ?? 0,
-                  onTap: _onItemTapped,
-                ),
-                MenuItem(
-                  label: '',
-                  selectedIcon: IconsaxBold.scan,
-                  unselectedIcon: IconsaxOutline.scan,
-                  index: 2,
-                  selectedPageIndex: widget.selectedIndex ?? 0,
-                  onTap: _onItemTapped,
-                ),
-                MenuItem(
-                  label: 'History',
-                  selectedIcon: IconsaxBold.document_text,
-                  unselectedIcon: IconsaxOutline.document_text,
-                  index: 3,
-                  selectedPageIndex: widget.selectedIndex ?? 0,
-                  onTap: _onItemTapped,
-                ),
-                MenuItem(
-                  label: 'Profile',
-                  selectedIcon: IconsaxBold.user,
-                  unselectedIcon: IconsaxOutline.user,
-                  index: 4,
-                  selectedPageIndex: widget.selectedIndex ?? 0,
-                  onTap: _onItemTapped,
-                ),
-              ],
-            )),
+              );
+            },
+            backgroundColor:
+                neutralTheme, // Ganti dengan warna tema yang sesuai
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(30), // Sesuaikan radius tombol
+            ),
+            child: const Icon(
+              IconsaxOutline.scan, // Ikon scan
+              size: 28, // Ukuran ikon
+              color: Colors.white, // Warna ikon
+            ),
+          ),
+        ),
       ),
     );
   }
