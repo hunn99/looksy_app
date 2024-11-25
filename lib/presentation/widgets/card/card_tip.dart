b26aeb5b38779ef51dba8f4965c6815d1a4c5923
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-11-25 13:26:32 +0700

    [feat] Add widget CardTip

---
+++ lib/presentation/widgets/card/card_tip.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:looksy_app/presentation/utils/theme.dart';
+
+class TipCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String subtitle;
+  final VoidCallback onTap;
+
+  const TipCard({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        elevation: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(10)),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    subtitle,
+                    style: TextStyle(fontSize: 12, color: neutralTheme[300]!),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
