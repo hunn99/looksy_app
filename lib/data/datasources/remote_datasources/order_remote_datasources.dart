5bbf5d359f9dd03b7e1da4d2b7e6f8b253dcb040
hunn99 <dennymalikibrahim@gmail.com>
2024-11-26 17:55:10 +0700

    [fix] Order remote

--- lib/data/datasources/remote_datasources/order_remote_datasources.dart
+++ lib/data/datasources/remote_datasources/order_remote_datasources.dart
@@ -1,51 +1,45 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+import 'package:looksy_app/data/datasources/remote_datasources/auth_remote_datasources.dart';
+import 'package:looksy_app/data/dto/requests/order_dto.dart';
+import 'package:looksy_app/domain/entities/order.dart' as entities;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class OrderServices {
+  final String baseUrl =
+      'http://192.168.71.10:8000/api'; // Gunakan base URL yang konsisten
   // Fungsi untuk membuat order
-  Future<void> createOrder({
-    required String date,
-    required String time,
-    required double totalPayment,
-    required List<Map<String, dynamic>> pickedServices, // Tambahkan parameter ini
-  }) async {
+  Future<Either<String, entities.Order>> order(OrderDto params) async {
+    final url = Uri.parse('$baseUrl/orders');
     try {
+      print(url);
+      // Cetak data JSON yang akan dikirim
+      final body = jsonEncode(params.toJson());
+      print("Data yang dikirim ke backend: $body");
       // Ambil token dari SharedPreferences
       final prefs = await SharedPreferences.getInstance();
       final token = prefs.getString('auth_token');
 
-      if (token == null) {
-        throw Exception('Token tidak ditemukan, harap login kembali');
-      }
-
-      // Kirim permintaan ke API
-      final url = Uri.parse('http://localhost:8000/api/orders');
       final response = await http.post(
         url,
         headers: {
+          'Authorization': 'Bearer $token',
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token', // Sertakan token autentikasi
         },
-        body: jsonEncode({
-          'date': date,
-          'time': time,
-          'total_payment': totalPayment,
-          'services': pickedServices.map((service) => service["id"]).toList(),
-        }),
+        body: body,
       );
-
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
+      print("Status Code: ${response.statusCode}");
+      print("Response Body: ${response.body}");
 
       if (response.statusCode == 201) {
-        print('Order created successfully');
+        return Right(
+            entities.Order.fromJson(jsonDecode(response.body)['data']));
       } else {
-        final responseData = jsonDecode(response.body);
-        throw Exception('Gagal membuat order: ${responseData['error'] ?? 'Unknown error'}');
+        return Left(jsonDecode(response.body)['error']);
       }
-    } catch (error) {
-      throw Exception('Error creating order: $error');
+    } catch (e) {
+      return Left('Error during order: $e');
     }
   }
 }
