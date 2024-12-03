bf9d3ab54b772ade22341df40347c6130713910e
satriaabraarr <satria.abrarr@gmail.com>
2024-12-03 11:49:53 +0700

    [feat] Convert time format

--- lib/data/dto/requests/order_dto.dart
+++ lib/data/dto/requests/order_dto.dart
@@ -14,10 +14,26 @@ class OrderDto {
   Map<String, dynamic> toJson() {
     return {
       'date': date,
-      'time': time,
+      'time': _convertTimeTo24HourFormat(time),
       'total_payment': totalPayment,
       'services': pickedServices,
     };
+  }
+
+  String _convertTimeTo24HourFormat(String time12Hour) {
+    final timeParts = time12Hour.split(" ");
+    final hourMinute = timeParts[0].split(":");
+    int hour = int.parse(hourMinute[0]);
+    final minute = hourMinute[1];
+    final isPM = timeParts[1].toUpperCase() == 'PM';
+
+    if (isPM && hour != 12) {
+      hour += 12;
+    } else if (!isPM && hour == 12) {
+      hour = 0;
+    }
+
+    return '${hour.toString().padLeft(2, '0')}:$minute';
   }
 
   OrderDto copyWith({
