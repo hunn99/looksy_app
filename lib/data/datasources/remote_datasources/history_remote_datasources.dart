6b702e00382544aa235772ef416ffcd0eb58736c
satriaabraarr <satria.abrarr@gmail.com>
2024-12-03 11:49:11 +0700

    [feat] Add history remote datasources

---
+++ lib/data/datasources/remote_datasources/history_remote_datasources.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:dartz/dartz.dart' as dartz;
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:looksy_app/domain/entities/order.dart';
+
+class HistoryRemoteDataSource {
+  final String baseUrl = 'http://localhost:8000/api';
+
+  Future<dartz.Either<String, List<Order>>> getOrderHistory() async {
+    final url = Uri.parse('$baseUrl/history');
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('auth_token');
+
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+        print('Response Data: $body');
+        final List<dynamic> data = jsonDecode(response.body)['data'];
+        final orders = data.map((e) => Order.fromJson(e)).toList();
+        return dartz.Right(orders);
+      } else {
+        return dartz.Left(jsonDecode(response.body)['error']);
+      }
+    } catch (e) {
+      return dartz.Left('Error fetching order history: $e');
+    }
+  }
+  
+  Future<dartz.Either<String, bool>> cancelOrder(int orderId) async {
+    final url = Uri.parse('$baseUrl/history/cancel/$orderId');
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('auth_token');
+
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return dartz.Right(true);
+      } else {
+        final error = jsonDecode(response.body)['error'] ?? 'Failed to cancel order';
+        return dartz.Left(error);
+      }
+    } catch (e) {
+      return dartz.Left('Error canceling order: $e');
+    }
+  }
+}
