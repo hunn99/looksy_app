17dacb7b1f061e57ae1149ec2b5cb3053a57df14
hunn99 <dennymalikibrahim@gmail.com>
2024-12-05 15:23:40 +0700

    [fix] Use constant for baseURL

--- lib/data/datasources/remote_datasources/order_remote_datasources.dart
+++ lib/data/datasources/remote_datasources/order_remote_datasources.dart
@@ -4,16 +4,25 @@ import 'package:http/http.dart' as http;
 import 'package:looksy_app/data/dto/requests/order_dto.dart';
 import 'package:looksy_app/domain/entities/order.dart' as entities;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:looksy_app/presentation/utils/constants.dart';
+
 
 class OrderServices {
-  final String baseUrl =
-      'http://192.168.64.142:8000/api'; // Gunakan base URL yang konsisten
+  // final String baseUrl =
+  //     'http://192.168.23.251:8000/api'; // Gunakan base URL yang konsisten
   // Fungsi untuk membuat order
   Future<Either<String, entities.Order>> order(OrderDto params) async {
-    final url = Uri.parse('$baseUrl/orders');
+    final url = Uri.parse('$baseUrl/api/orders');
     try {
+      print('1');
       print(url);
+      print('2');
       // Cetak data JSON yang akan dikirim
+      print(params.date);
+      print(params.time);
+      print(params.totalPayment);
+      print(params.pickedServices);
+
       final body = jsonEncode(params.toJson());
       print("Data yang dikirim ke backend: $body");
       // Ambil token dari SharedPreferences
