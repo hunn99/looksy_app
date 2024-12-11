88f51987289253cf42dd6ac6ed4c5084a5097a47
hunn99 <dennymalikibrahim@gmail.com>
2024-12-11 20:05:08 +0700

    [fix] Add empthy state, can select recomendation, change layout

--- lib/presentation/pages/scan/result.dart
+++ lib/presentation/pages/scan/result.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 import 'package:ficonsax/ficonsax.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:looksy_app/domain/entities/hairstyle.dart';
 import 'package:looksy_app/presentation/bloc/scan/scan_bloc.dart';
 import 'package:looksy_app/presentation/pages/scan/detail.dart';
 import 'package:looksy_app/presentation/utils/text.dart';
 import 'package:looksy_app/presentation/utils/theme.dart';
 import 'package:looksy_app/presentation/widgets/buttons/button.dart';
-import 'package:looksy_app/presentation/widgets/card/card_recommendation.dart'; // Pastikan untuk mengimpor ScanBloc
+import 'package:looksy_app/presentation/widgets/card/card_recommendation.dart';
+import 'package:looksy_app/presentation/widgets/empthy/noRecommendation.dart'; // Pastikan untuk mengimpor ScanBloc
 
 class ResultPage extends StatefulWidget {
   final File images;
@@ -20,43 +20,51 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  int? selectedIndex;
+
   @override
   void initState() {
     super.initState();
-    print(widget.images);
     // Triggering the event to start the image upload and face shape detection
     context.read<ScanBloc>().add(UploadImageEvent(widget.images));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(IconsaxOutline.arrow_left, color: Colors.white),
-          onPressed: () => Navigator.pop(context),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(IconsaxOutline.arrow_left, color: Colors.white),
+            onPressed: () => Navigator.pop(context),
+          ),
+          title: const Text('Result', style: TextStyle(color: Colors.white)),
+          backgroundColor: neutralTheme,
+          elevation: 0,
         ),
-        title: const Text('Result', style: TextStyle(color: Colors.white)),
-        backgroundColor: neutralTheme,
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            child: Center(
+        body: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              padding: const EdgeInsets.all(24),
               child: BlocBuilder<ScanBloc, ScanState>(
                 builder: (context, state) {
-                  return Column(
+                  return Row(
                     children: [
-                      SizedBox(
+                      Container(
                         width: 180,
-                        height: 220,
+                        height: double
+                            .infinity, // Tentukan ukuran yang sesuai jika perlu
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
                         child: Transform(
                           alignment: Alignment.center,
                           transform: Matrix4.rotationY(math.pi),
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
+                            borderRadius: BorderRadius.circular(24),
                             child: Image.file(
                               widget.images,
                               fit: BoxFit.cover,
@@ -64,110 +72,136 @@ class _ResultPageState extends State<ResultPage> {
                           ),
                         ),
                       ),
-                      const SizedBox(height: 12),
-                      Text('Face Shape', style: bodyGrey2),
-                      state is ScanLoading // Show loading spinner when scanning
-                          ? const CircularProgressIndicator()
-                          : state is ScanFailed // Show error message if failed
-                              ? const Text(
-                                  'Wajah tidak terdeteksi',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w500,
-                                    color: redTheme,
-                                  ),
-                                )
-                              : state is ScanSuccess // Show face shape if successful
-                                  ? Text(
-                                      state.hairstyles.first.faceShape,
-                                      style: heading3Black,
-                                    )
-                                  : Text(
-                                      'Tidak ada hasil',
-                                      style: heading3Black,
-                                    ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Face Shape', style: bodyGrey2),
+                            state is ScanLoading // Show loading spinner when scanning
+                                ? const CircularProgressIndicator()
+                                : state is ScanFailed // Show error message if failed
+                                    ? const Text(
+                                        'No Face Shape Detected',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w500,
+                                          color: redTheme,
+                                        ),
+                                      )
+                                    : state is ScanSuccess // Show face shape if successful
+                                        ? Text(
+                                            state.hairstyles.first.faceShape,
+                                            style: headingBlack,
+                                          )
+                                        : Text(
+                                            'Tidak ada hasil',
+                                            style: heading3Black,
+                                          ),
+                          ],
+                        ),
+                      ),
                     ],
                   );
                 },
               ),
             ),
-          ),
-          BlocBuilder<ScanBloc, ScanState>(
-            builder: (context, state) {
-              if (state is ScanLoading) {
-                // Tampilkan indikator loading
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is ScanFailed) {
-                // Tampilkan pesan error
-                return const Center(
-                  child: Text(
-                    'Wajah tidak terdeteksi',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: redTheme,
+            BlocBuilder<ScanBloc, ScanState>(
+              builder: (context, state) {
+                if (state is ScanLoading) {
+                  // Tampilkan indikator loading
+                  return Expanded(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: const EdgeInsets.all(16),
+                      color: neutralTheme,
+                      child: const Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.white), // Warna putih
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              } else if (state is ScanSuccess) {
-                // Tampilkan hasil scan dengan daftar hairstyle
-                return Expanded(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: const EdgeInsets.all(16),
-                    color: neutralTheme,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Choose your hairstyle',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
+                  );
+                } else if (state is ScanFailed) {
+                  // Tampilkan pesan error
+                  return const NoRecommendationFound();
+                } else if (state is ScanSuccess) {
+                  // Tampilkan hasil scan dengan daftar hairstyle
+                  return Expanded(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: const EdgeInsets.all(16),
+                      color: neutralTheme,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Choose your hairstyle',
+                            style: headingWhite,
                           ),
-                        ),
-                        const SizedBox(height: 16),
-                        Expanded(
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: state.hairstyles.length,
-                            itemBuilder: (context, index) {
-                              final hairstyle = state.hairstyles[index];
-                              return HairstyleItem(
-                                imageUrl: hairstyle.photo,
-                                hairstyleName: hairstyle.hairStyle,
-                              );
-                            },
+                          const SizedBox(height: 16),
+                          Expanded(
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: state.hairstyles.length,
+                              itemBuilder: (context, index) {
+                                final hairstyle = state.hairstyles[index];
+                                bool isSelected = selectedIndex == index;
+
+                                return Row(
+                                  children: [
+                                    HairstyleItem(
+                                      imageUrl: hairstyle.photo,
+                                      hairstyleName: hairstyle.hairStyle,
+                                      isSelected: isSelected,
+                                      onSelect: () {
+                                        setState(() {
+                                          selectedIndex = index;
+                                        });
+                                      },
+                                      index: index,
+                                    ),
+                                    const SizedBox(width: 12),
+                                  ],
+                                );
+                              },
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 24),
-                        LargeFillButtonWhite(
-                          label: 'Next',
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HairStyleDetailPage(
-                                    hairstyle: state.hairstyles.first),
-                              ),
-                            );
-                          },
-                        ),
-                      ],
+                          const SizedBox(height: 24),
+                          LargeFillButtonWhite(
+                            label: 'Choose',
+                            onPressed: selectedIndex != null
+                                ? () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            HairStyleDetailPage(
+                                          hairstyle: state
+                                              .hairstyles[selectedIndex ?? 0],
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                : () {}, // Disable button if no card is selected
+                            isDisabled: selectedIndex ==
+                                null, // Disable button if no selection
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              } else {
-                // Kondisi default jika tidak ada data
-                return const Center(child: Text('Tidak ada hasil.'));
-              }
-            },
-          ),
-        ],
+                  );
+                } else {
+                  // Kondisi default jika tidak ada data
+                  return const Center(child: Text('Tidak ada hasil.'));
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
-
-  // Helper method to build hairstyle items
 }
