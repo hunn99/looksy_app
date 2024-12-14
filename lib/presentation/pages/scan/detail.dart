f04774fef911b5d60d574eae7af54d9a14dded2a
hunn99 <dennymalikibrahim@gmail.com>
2024-12-14 17:06:03 +0700

    [fix] Use bloc in detail recommendation

--- lib/presentation/pages/scan/detail.dart
+++ lib/presentation/pages/scan/detail.dart
@@ -1,10 +1,13 @@
 import 'package:ficonsax/ficonsax.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:looksy_app/domain/entities/hairstyle.dart';
+import 'package:looksy_app/presentation/bloc/hairstyle/hairstyle_bloc.dart';
 import 'package:looksy_app/presentation/pages/profile/save_page.dart';
 import 'package:looksy_app/presentation/utils/text.dart';
 import 'package:looksy_app/presentation/utils/theme.dart';
 import 'package:looksy_app/presentation/widgets/buttons/button.dart';
+import 'package:looksy_app/presentation/widgets/modals/dialogsave.dart';
 
 class HairStyleDetailPage extends StatefulWidget {
   final HairStyle hairstyle;
@@ -79,79 +82,142 @@ class _HairStyleDetailPageState extends State<HairStyleDetailPage> {
               ),
             ),
             Expanded(
-                child: Container(
-              width: double.infinity,
-              color: Colors.white,
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Characteristics',
-                            style: bodyGrey2,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            widget.hairstyle.characteristics,
-                            style: heading4Black,
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          Text(
-                            'Face Suitability',
-                            style: bodyGrey2,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            widget.hairstyle.faceSuitability,
-                            style: heading4Black,
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          Text(
-                            'Maintenance',
-                            style: bodyGrey2,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            widget.hairstyle.maintenance,
-                            style: heading4Black,
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          Text(
-                            'Impression',
-                            style: bodyGrey2,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            widget.hairstyle.impression,
-                            style: heading4Black,
-                          ),
-                        ],
+              child: Container(
+                width: double.infinity,
+                color: Colors.white,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Characteristics',
+                              style: bodyGrey2,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              widget.hairstyle.characteristics,
+                              style: heading4Black,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Text(
+                              'Face Suitability',
+                              style: bodyGrey2,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              widget.hairstyle.faceSuitability,
+                              style: heading4Black,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Text(
+                              'Maintenance',
+                              style: bodyGrey2,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              widget.hairstyle.maintenance,
+                              style: heading4Black,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Text(
+                              'Impression',
+                              style: bodyGrey2,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              widget.hairstyle.impression,
+                              style: heading4Black,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  LargeFillButton(
-                    label: 'Next',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SavePage()),
-                      );
-                    },
-                  )
-                ],
+                  ],
+                ),
               ),
-            ))
+            ),
+            BlocConsumer<HairstyleBloc, HairstyleState>(
+              listener: (context, state) {
+                if (state is HairstyleSuccess) {
+                  print(state.buttonType);
+                  if (state.buttonType == 'save') {
+                    showDialog(
+                      context: context,
+                      builder: (_) => SaveDialog(),
+                    );
+                  } else if (state.buttonType == 'save_booking') {
+                    showDialog(
+                      context: context,
+                      builder: (_) => SaveBookingDialog(),
+                    );
+                  }
+                }
+
+                if (state is HairstyleFailed) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                          'Failed to save hairstyle: ${state.errorMessage}'),
+                      behavior: SnackBarBehavior.floating,
+                      margin: const EdgeInsets.only(
+                        bottom: 80,
+                        left: 16,
+                        right: 16,
+                      ),
+                      backgroundColor: Colors.red[600],
+                      duration: const Duration(seconds: 3),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: LargeOutlineButton(
+                          label: 'Save',
+                          onPressed: () {
+                            context.read<HairstyleBloc>().add(
+                                  SaveHairstyleEvent(
+                                    hairstyleId: widget.hairstyle.id,
+                                    buttonType: 'save',
+                                  ),
+                                );
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      Expanded(
+                        child: LargeFillButton(
+                          label: 'Save & Booking',
+                          onPressed: () {
+                            context.read<HairstyleBloc>().add(
+                                  SaveHairstyleEvent(
+                                    hairstyleId: widget.hairstyle.id,
+                                    buttonType: 'save_booking',
+                                  ),
+                                );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            )
           ],
         ),
       ),
