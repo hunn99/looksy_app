ee8ccd7b0d79abfd5a444d6c768b00874c715377
hunn99 <dennymalikibrahim@gmail.com>
2024-12-14 17:07:30 +0700

    [fix] Take data for save page

--- lib/presentation/pages/profile/save_page.dart
+++ lib/presentation/pages/profile/save_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:looksy_app/presentation/pages/profile/save_detail.dart';
 import 'package:looksy_app/presentation/utils/theme.dart';
 import 'package:looksy_app/presentation/widgets/card/card_tips.dart';
+
+import '../../bloc/hairstyle/hairstyle_bloc.dart';
+import '../navigation/navigation.dart';
 
 class SavePage extends StatefulWidget {
   const SavePage({Key? key}) : super(key: key);
@@ -11,46 +15,52 @@ class SavePage extends StatefulWidget {
 }
 
 class _SavePageState extends State<SavePage> {
-  List<Hairstyle> hairstyles = [
-    Hairstyle(
-      name: "Buzz Cut",
-      faceShape: "Square",
-      imageUrl: "assets/images/Wavy.jpg",
-      characteristics:
-          "The Buzz Cut is characterized by very short and uniform hair length all over the head. This style offers a clean, neat appearance with sharp and defined hairlines.",
-      faceSuitability:
-          "This style is most suitable for oval, square, or triangular face shapes, as the short cut accentuates facial bone structure and the jawline.",
-      maintenance:
-          "The Buzz Cut requires minimal upkeep. Simply keep the scalp clean and moisturized to prevent dryness. A trim every 1-2 weeks helps maintain its neat look.",
-      impression:
-          "The Buzz Cut gives off a masculine, practical, and bold look. It’s often seen as a symbol of a minimalist yet stylish appearance.",
-    ),
-    Hairstyle(
-      name: "Pompadour",
-      faceShape: "Oval",
-      imageUrl: "assets/images/Wavy.jpg",
-      characteristics:
-          "The Pompadour is a classic style that adds volume to the top while keeping the sides short. It’s bold and timeless.",
-      faceSuitability:
-          "This style works best for oval or square faces, enhancing the balance of facial features.",
-      maintenance:
-          "Requires regular styling and occasional trimming to maintain volume and neatness.",
-      impression:
-          "The Pompadour gives off a sophisticated yet daring impression, ideal for those who love vintage-inspired looks.",
-    ),
-  ];
+  // List<Hairstyle> hairstyles = [
+  //   Hairstyle(
+  //     name: "Buzz Cut",
+  //     faceShape: "Square",
+  //     imageUrl: "assets/images/Wavy.jpg",
+  //     characteristics:
+  //         "The Buzz Cut is characterized by very short and uniform hair length all over the head. This style offers a clean, neat appearance with sharp and defined hairlines.",
+  //     faceSuitability:
+  //         "This style is most suitable for oval, square, or triangular face shapes, as the short cut accentuates facial bone structure and the jawline.",
+  //     maintenance:
+  //         "The Buzz Cut requires minimal upkeep. Simply keep the scalp clean and moisturized to prevent dryness. A trim every 1-2 weeks helps maintain its neat look.",
+  //     impression:
+  //         "The Buzz Cut gives off a masculine, practical, and bold look. It’s often seen as a symbol of a minimalist yet stylish appearance.",
+  //   ),
+  //   Hairstyle(
+  //     name: "Pompadour",
+  //     faceShape: "Oval",
+  //     imageUrl: "assets/images/Wavy.jpg",
+  //     characteristics:
+  //         "The Pompadour is a classic style that adds volume to the top while keeping the sides short. It’s bold and timeless.",
+  //     faceSuitability:
+  //         "This style works best for oval or square faces, enhancing the balance of facial features.",
+  //     maintenance:
+  //         "Requires regular styling and occasional trimming to maintain volume and neatness.",
+  //     impression:
+  //         "The Pompadour gives off a sophisticated yet daring impression, ideal for those who love vintage-inspired looks.",
+  //   ),
+  // ];
 
-  List<Hairstyle> savedHairstyles = [];
+  // List<Hairstyle> savedHairstyles = [];
 
-  void toggleSave(Hairstyle hairstyle) {
-    setState(() {
-      if (savedHairstyles.contains(hairstyle)) {
-        savedHairstyles.remove(hairstyle);
-      } else {
-        savedHairstyles.add(hairstyle);
-      }
-    });
-  }
+  // @override
+  // void initState() {
+  //   context.read<HairstyleBloc>().add(const GetHairstyleEvent());
+  //   super.initState();
+  // }
+
+  // void toggleSave(Hairstyle hairstyle) {
+  //   setState(() {
+  //     if (savedHairstyles.contains(hairstyle)) {
+  //       savedHairstyles.remove(hairstyle);
+  //     } else {
+  //       savedHairstyles.add(hairstyle);
+  //     }
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -65,47 +75,75 @@ class _SavePageState extends State<SavePage> {
             color: Colors.white,
           ),
         ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back), // Ikon kembali
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const NavigationPage(
+                initialIndex: 3,
+              ),
+            ));
+          },
+        ),
         backgroundColor: neutralTheme,
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          physics: const BouncingScrollPhysics(),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 0.75,
-          ),
-          itemCount: hairstyles.length,
-          itemBuilder: (context, index) {
-            final hairstyle = hairstyles[index];
-            bool isSaved = savedHairstyles.contains(hairstyle);
-            return TipsCard(
-              imagePath: hairstyle.imageUrl,
-              title: hairstyle.name,
-              subtitle: "For ${hairstyle.faceShape} Face",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HairstyleDetailPage(
-                      faceShape: hairstyle.faceShape,
-                      hairstyleName: hairstyle.name,
-                      imageUrl: hairstyle.imageUrl,
-                      characteristics: hairstyle.characteristics,
-                      faceSuitability: hairstyle.faceSuitability,
-                      maintenance: hairstyle.maintenance,
-                      impression: hairstyle.impression,
-                      //onSave: () => toggleSave(hairstyle), // Pass the save toggle function
-                    ),
-                  ),
-                );
-              },
+      body: BlocBuilder<HairstyleBloc, HairstyleState>(
+        builder: (context, state) {
+          if (state is HairstyleLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is HairstyleFailed) {
+            return Center(
+              child: Text(
+                state.errorMessage,
+                style: const TextStyle(color: Colors.red),
+              ),
             );
-          },
-        ),
+          }
+          if (state is HairstyleSuccess) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                physics: const BouncingScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: state.hairstyle.length,
+                itemBuilder: (context, index) {
+                  final hairstyle = state.hairstyle[index];
+                  // bool isSaved = savedHairstyles.contains(hairstyle);
+                  return TipsCard(
+                    imagePath: hairstyle.photo,
+                    title: hairstyle.hairStyle,
+                    subtitle: "For ${hairstyle.faceShape} Face",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HairstyleDetailPage(
+                            faceShape: hairstyle.faceShape,
+                            hairstyleName: hairstyle.hairStyle,
+                            imageUrl: hairstyle.photo,
+                            characteristics: hairstyle.characteristics,
+                            faceSuitability: hairstyle.faceSuitability,
+                            maintenance: hairstyle.maintenance,
+                            impression: hairstyle.impression,
+                            //onSave: () => toggleSave(hairstyle), // Pass the save toggle function
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            );
+          }
+          return Container();
+        },
       ),
     );
   }
